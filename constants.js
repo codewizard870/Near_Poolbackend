e17d1e0024986438936c02befa39015b04b00b38
alenzer <alenzer0902@gmail.com>
2022-05-31 13:30:01 -0400

    init

---
+++ constants.js
@@ -0,0 +1,146 @@
+const crypto = require('crypto');
+
+function encrypt3DES(data, key) {
+ const md5Key = crypto.createHash('md5').update(key).digest("hex").substr(0, 24);
+ const cipher = crypto.createCipheriv('des-ede3', md5Key, '');
+
+ let encrypted = cipher.update(data, 'utf8', 'base64');
+ encrypted += cipher.final('base64');
+ return encrypted;
+}
+
+function decrypt3DES(data, key) {
+ const md5Key = crypto.createHash('md5').update(key).digest("hex").substr(0, 24);
+ const decipher = crypto.createDecipheriv('des-ede3', md5Key, '');
+
+ let encrypted = decipher.update(data, 'base64', 'utf8');
+ encrypted += decipher.final('utf8');
+ return encrypted;
+}
+
+// const TERRA_SEED = decrypt3DES("wNGkDc9ZSAiWm4Y2L+Fog2xev8DC/hJLoiEWB7m01PGIYye/McmorgyAz1NiNiEZjRZXKY+Cd8Gut8pCIYMkZICPwohWzk0pwI9X/9xeR1j2Lh8y3ZmhP1o+/6huNG3LfDtECahzO0Ngnl2Xl2u2ugMYJgEIKYoZYZ7Yj16Pjhg8lnp5K+Ts/Pxj8Pyx3EMdQiHGRJa/X/fit+Sp58pJIA==", "Thisisaprettyquity");
+
+const CONTRACT_NAME = 'pool.alenzertest.testnet'; /* TODO: fill this in! */
+function getConfig (env) {
+  switch (env) {
+    case 'production':
+    case 'mainnet':
+      return {
+        networkId: 'mainnet',
+        nodeUrl: 'https://rpc.mainnet.near.org',
+        contractName: CONTRACT_NAME,
+        walletUrl: 'https://wallet.near.org',
+        helperUrl: 'https://helper.mainnet.near.org'
+      }
+    case 'development':
+    case 'testnet':
+      return {
+        networkId: 'testnet',
+        nodeUrl: 'https://rpc.testnet.near.org',
+        contractName: CONTRACT_NAME,
+        walletUrl: 'https://wallet.testnet.near.org',
+        helperUrl: 'https://helper.testnet.near.org'
+      }
+    default:
+      throw Error(`Unconfigured environment '${env}'. Can be configured in src/config.js.`)
+  }
+}
+
+const StableCoins=[
+  {
+    name: 'USDC',
+    id: 'usd-coin',
+    description: 'USD Coin',
+    avatar: 'Usdc.svg',
+    apr: 14.87,
+    tvl_coin: 47243320,
+    tvl_usd: 47243320,
+    stable: true,
+    upcoming: false
+  },
+  {
+    name: 'USDT',
+    id: 'tether',
+    description: 'USD Tether',
+    avatar: 'Usdt.svg',
+    apr: 14.87,
+    tvl_coin: 47243320,
+    tvl_usd: 47243320, 
+    stable: true,
+    upcoming: false
+  },
+  {
+    name: 'DAI',
+    id: 'dai',
+    description: 'Dai',
+    avatar: 'Dai.svg',
+    apr: 14.87,
+    tvl_coin: 47243320,
+    tvl_usd: 47243320, 
+    stable: true,
+    upcoming: false
+  },
+  {
+    name: 'USN',
+    id: 'usn',
+    description: 'USD NEAR',
+    avatar: 'Usn.svg',
+    apr: 14.87,
+    tvl_coin: 47243320,
+    tvl_usd: 47243320, 
+    stable: true,
+    upcoming: false
+  },
+  {
+    name: 'wBTC',
+    id: 'wrapped-bitcoin',
+    description: 'Wrapped Bitcoin',
+    avatar: 'Wbtc.svg',
+    apr: 9.87,
+    tvl_coin: 47243320,
+    tvl_usd: 47243320, 
+    stable: false,
+    upcoming: false
+  },
+  {
+    name: 'ETH',
+    id: 'ethereum',
+    description: 'Ethereum',
+    avatar: 'Eth.png',
+    apr: 9.87,
+    tvl_coin: 47243320,
+    tvl_usd: 47243320, 
+    stable: false,
+    upcoming: false
+  },
+  {
+    name: 'wNEAR',
+    id: 'wrapped-near',
+    description: 'Wrapped Near',
+    avatar: 'Wnear.svg',
+    apr: 9.87,
+    tvl_coin: 47243320,
+    tvl_usd: 47243320, 
+    stable: false,
+    upcoming: false
+  },
+  {
+    name: 'NEARt',
+    description: 'Near Treasury (Cooming Soon)',
+    avatar: 'Neart.svg',
+    apr: 9.87,
+    tvl_coin: 47243320,
+    tvl_usd: 47243320, 
+    stable: false,
+    upcoming: true
+  }
+]
+
+
+module.exports = {
+  CONTRACT_NAME,
+  StableCoins,
+  getConfig,
+  encrypt3DES,
+  decrypt3DES
+}
---
+++ index.js
@@ -0,0 +1,235 @@
+const axios = require('axios');
+
+const CONTRACT_NAME = require("./constants").CONTRACT_NAME;
+const StableCoins = require("./constants").StableCoins;
+const encrypt3DES = require("./constants").encrypt3DES;
+const decrypt3DES = require("./constants").decrypt3DES;
+const getConfig = require("./constants").getConfig;
+
+const port = process.env.PORT || 3001
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const fs = require('fs');
+const path = require("path");
+const { keyStores, KeyPair, connect, Contract } = require("near-api-js");
+
+var near;
+var formidable = require('formidable');
+
+app.use(express.json());
+app.use(cors());
+
+// const args = process.argv.slice(2)
+
+// let encrypt = encrypt3DES(args[0], "Thisisaprettyquity");
+// console.log(encrypt);
+
+// let decrypt = decrypt3DES(encrypt, "Thisisaprettyquity");
+// console.log(decrypt);
+
+async function init(){
+  const keyStore = new keyStores.InMemoryKeyStore();
+  const PRIVATE_KEY =
+    "2nYhYbV58SqjgpmD5QwCjg9EWFMS9P3JCap9U58Wn651cCPtxKxYHmSk6oSZh2SeYFohcyoe8zQBbGNvRNvdAQoz";
+  const keyPair = KeyPair.fromString(PRIVATE_KEY);
+  await keyStore.setKey("testnet", "staking_treasury.testnet", keyPair);
+
+  // const nearConfig = getConfig("testnet");
+  const config = {
+    networkId: "testnet",
+    keyStore, 
+    nodeUrl: "https://rpc.testnet.near.org",
+    walletUrl: "https://wallet.testnet.near.org",
+    helperUrl: "https://helper.testnet.near.org",
+    explorerUrl: "https://explorer.testnet.near.org",
+  };
+  near = await connect(
+    config
+  );
+}
+async function withdraw(sender, amount, coinType) {
+  let coins = StableCoins.filter((coin) => coin.upcoming == false);
+  const price = [1, 1, 1, 1, 1, 1, 1];
+  for(let i=0; i<coins.length; i++){
+    let res
+    try {
+      res = await axios.get(
+        `https://api.coingecko.com/api/v3/simple/price?ids=${coins[i].id}&vs_currencies=usd`
+      );
+    } catch (e) { }
+
+    price[i] = Math.floor(res.data[`${coins[i].id}`]["usd"] * 100);
+  }
+
+  let withdraw_msg = {
+    account: sender,
+    coin: coinType,
+    amount: amount,
+    price: price
+  }
+
+  const account = await near.account("staking_treasury.testnet");
+  const contract = new Contract(
+    account, // the account object that is connecting
+    CONTRACT_NAME,
+    {
+      viewMethods: ["get_status"],
+      changeMethods: ["withdraw"],
+    }
+  );
+
+  try{
+    await contract.withdraw(withdraw_msg);
+    return "success";
+  }
+  catch(e){
+    return "failed"
+  }
+}
+init();
+withdraw('alenzertest.testnet', 1000, 'wBTC');
+
+app.post("/withdraw", async function (req, res) {
+  var form = new formidable.IncomingForm();
+  form.parse(req, async function (err, fields, files) {
+    let count = 0;
+    let result;
+    do {
+      result = await withdraw(fields.account, fields.amount, fields.coinType);
+      console.log(result);
+      await sleep(1000);
+      count++;
+    } while (result != 'success' && count < 10)
+
+    if (result == 'success') {
+      res.status(200).jsonp({
+        data: "success"
+      });
+    } else{
+      res.status(500).jsonp({
+        data: result
+      })
+    }
+  })
+});
+
+
+async function payReward() {
+  const account = await near.account("staking_treasury.testnet");
+  const contract = new Contract(
+    account, // the account object that is connecting
+    CONTRACT_NAME,
+    {
+      viewMethods: ["get_status"],
+      changeMethods: ["reward"],
+    }
+  );
+
+  try{
+    await contract.reward();
+    console.log("reward success")
+    return "success";
+  }
+  catch(e){
+    console.log("reward failed");
+    return "failed"
+  }
+}
+
+async function farm() {
+  let coins = StableCoins.filter((coin) => coin.upcoming == false);
+  const price = [1, 1, 1, 1, 1, 1, 1];
+  for(let i=0; i<coins.length; i++){
+    let res
+    try {
+      res = await axios.get(
+        `https://api.coingecko.com/api/v3/simple/price?ids=${coins[i].id}&vs_currencies=usd`
+      );
+    } catch (e) { }
+
+    price[i] = Math.floor(res.data[`${coins[i].id}`]["usd"] * 100);
+  }
+  const account = await near.account("staking_treasury.testnet");
+  const contract = new Contract(
+    account, // the account object that is connecting
+    CONTRACT_NAME,
+    {
+      viewMethods: ["get_status"],
+      changeMethods: ["farm"],
+    }
+  );
+
+  try{
+    await contract.farm({price: price});
+    console.log("farm success");
+    return "success";
+  }
+  catch(e){
+    console.log("farm failed");
+    return "failed"
+  }
+}
+
+const nodeCron = require("node-cron");
+var job = nodeCron.schedule('*/10 * * * *', async function () {//m h day month dayOfweek
+  console.log("pay reward start")
+  let res = 'success';
+  let count = 0;
+  do {
+    res = await payReward();
+    await sleep(6000);
+    count++;
+  } while (res != 'success' && count < 10)
+
+  console.log("community farm start")
+  res = 'success';
+  count = 0;
+  do {
+    res = await farm();
+    await sleep(6000);
+    count++;
+  } while (res != 'success' && count < 10)
+});
+
+
+// async function potProcess() {
+//   let msg_pot = {
+//     "pot_process": {}
+//   };
+
+//   const pot = new MsgExecuteContract(
+//     mk.accAddress,
+//     poolAddress,
+//     msg_pot,
+//     {}
+//   )
+
+//   let res = await EstimateSend([pot], "pot_process");
+//   if (res == "success") console.log("pot process suceess");
+//   else console.log("pot process failed");
+//   return res;
+// }
+
+// var job2 = nodeCron.schedule('0 0 0 28 * *', async function () {//s m h day month dayOfweek
+//   console.log("Pot process start")
+//   let res = 'success';
+//   let count = 0;
+//   do {
+//     res = await potProcess();
+//     await sleep(6000);
+//     count++;
+//   } while (res != 'success' && count < 10)
+// });
+
+
+app.get('/', (req, res) => res.send("success v21"))
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`))
+
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
